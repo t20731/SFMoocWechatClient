009c660e3402938a208f77a5c85a2aca360985e5
smallsun <yanglingz610@sina.com>
2018-10-20 16:43:07 +0800

    initialize explore module

--- pages/explore/explore.js
+++ pages/explore/explore.js
@@ -1,6 +1,6 @@
-// pages/notice/editNotice.js
+// pages/notice/notice.js
 import Util from '../../utils/util';
-const app = getApp();
+const app = getApp()
 
 Page({
 
@@ -8,97 +8,87 @@ Page({
    * 页面的初始数据
    */
   data: {
-    noticeId: '',
-    content: ''
+    currentTab: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    if (options.noticeId != 'undefined'){
-      this.setData({
-        noticeId: options.noticeId,
-        content: options.noticeCont
-      });
-    }
   },
 
-  saveNotice: function (evt) {
-    let content = evt.detail.value.noticeCont;
+  init: function(){
+    let that = this;
     wx.request({
-      url: app.globalData.host + '/announcement/edit',
-      method: 'POST',
-      data: {
-        id: this.data.noticeId,
-        content: content,
-        createdBy: app.globalData.openId,
-        lastModifiedBy: app.globalData.openId
-      },
+      url: app.globalData.host + '/announcement/list',
+      method: 'GET',
       success: function (res) {
         if (res.data.msg === 'ok') {
-          Util.showToast('保存成功', 'success', 1000);
-          setTimeout(function () {
-            wx.reLaunch({
-              url: 'notice'
+          let notice = res.data.retObj[0];
+          if (notice) {
+            notice.lastModifiedDate = notice.lastModifiedDate.split(' ')[0];
+            that.setData({
+              notice: notice
             });
-          }, 1000);
-        }else{
-          Util.showToast('保存失败', 'none', 1000);
+          }
         }
       },
       fail: function (error) {
-        Util.showToast('保存失败', 'none', 1000);
+        console.log(error);
       }
     })
+  },
+
+  swichNav: function () {
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+     this.init();
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
