081c5768f311f4491ec1695b3ed2d736e8552391
Liu, Lance <lance.liu@sap.com>
2018-10-21 17:56:27 +0800

    Show event detail with fake data.

---
+++ pages/explore/eventDetail.js
@@ -0,0 +1,41 @@
+Page({
+  data: { // 参与页面渲染的数据
+    disabled: false,
+    loading: false,
+    registerBtnVal: 'Register',
+    eventDetail: {
+      user: {
+        id: '001',
+        nickname: 'Mickey White',
+        avatarUrl: 'https://developers.weixin.qq.com/miniprogram/dev/image/cat/0.jpg?t=18101919'
+      },
+      topic: 'Event Detail',
+      description: 'Bacon ipsum dolor amet shankle buffalo salami biltong, meatloaf pork strip steak meatball ham sausage chicken leberkas. Corned beef capicola picanha pork loin fatback hamburger. Leberkas short ribs pork chuck. Beef boudin turkey capicola, shankle ham hock frankfurter leberkas spare ribs shoulder ground round flank ham sirloin.',
+      startDate: '2018-10-21 15:00',
+      endDate: '2018-10-21 16:00',
+      location: 'PVG 03 1C'
+    }
+  },
+
+  onLoad: function () {
+    // 页面渲染后 执行
+  },
+
+  onRegister: function(event) {
+    // call API to register the event
+    console.log('Register: ', event);
+    this.setData({
+      disabled: !this.data.disabled,
+      loading: !this.data.loading,
+    });
+
+    var me = this;
+    setTimeout(function() {
+      wx.showToast({title: 'Successfully'});
+      me.setData({
+        loading: !me.data.loading,
+        registerBtnVal: 'Registered'
+      })
+    }, 2000);
+  }
+});
