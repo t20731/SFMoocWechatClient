731ef76a3f5e8da4b8f7c849b1cc77bef8e0df26
I319716 <vicky.ji@sap.com>
2018-12-13 15:09:25 +0800

    Support edit personal profile

---
+++ pages/profile/profile.js
@@ -0,0 +1,139 @@
+import Util from '../../utils/util';
+import WCache from '../../utils/wcache';
+import WXRequest from '../../utils/wxRequest';
+
+
+const app = getApp(); 
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo:{},
+    isLoading:false,
+    isModified: false,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    let that = this;
+    let userId = Util.getUserId();
+    if (userId) {
+      wx.request({
+        url: app.globalData.host + '/user/' + userId,
+        method: 'GET',
+        success: function (res) {
+          console.log(res.data);
+          that.setData({
+            userInfo: res.data.retObj
+          });
+        },
+        fail: function (e) {
+          Util.showToast('Failed to get user data', 'none', 2000);
+        }
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+
+  onSubmit: function (e) {
+    let value = e.detail.value;
+    let userDetail = this._buildUserDetail(value);
+
+    WXRequest.post('/user/edit', userDetail).then(res => {
+      if (res.data.msg === 'ok') {
+        Util.showToast('Success', 'success', 1000);
+        setTimeout(function () {
+          wx.navigateBack({
+            delta: 1
+          });
+        }, 1000);
+      }
+    }).catch(e => {
+      console.log(e);
+    });
+  },
+
+  _buildUserDetail: function (value) {
+
+    let userDetail = {
+      id:this.data.userInfo.id,
+      // gender: value.gender,
+      department: value.department,
+      blog: value.blog,
+      signature:value.signature,
+      github:value.github,
+      seat:value.seat
+    };
+    return userDetail;
+  },
+
+  bindAnyModify: function(e) {
+    if (!this.data.isModified) {
+      this.setData({
+        isModified: true
+      })
+    }
+  },
+
+  // bindGenderChange: function (e) {
+  //   this.inputChange('genderIndex', e.detail.value);
+  // },
+
+  // inputChange: function (name, value) {
+  //   this.setData({
+  //     [name]: value
+  //   });
+  // },
+})
