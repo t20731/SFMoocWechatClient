da69057190b10094e7db265209d1aa5cfd4d88c7
Sherry Zhu <sherry.zhu@sap.com>
2019-05-29 16:32:26 +0800

    Create qr code for each sesssion

--- pages/explore/explore.js
+++ pages/explore/explore.js
@@ -36,7 +36,7 @@ Page({
       isNoMoreData: false
     },
     userInfo: {},
-    hasUserInfo: false,
+    hasUserInfo: false
   },
 
   /**
@@ -117,9 +117,15 @@ Page({
   },
  
   goDetail: function (e) {
+    // let id = e.currentTarget.id;
+    // this.setData({
+    //   navUrl: '../session/eventDetail?id=' + id
+    // });
+    // this._doLoadQR();
+
     let id = e.currentTarget.id;
     wx.navigateTo({
-      url: '../session/eventDetail?id=' + id,
+      url: '../session/eventDetail?id=' + id
     })
   },
 
